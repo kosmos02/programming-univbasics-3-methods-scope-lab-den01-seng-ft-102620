6f76bbb2750114df8139deda6e5d86c23dc73524
Alexander Gabriel <alexandrgabe@gmail.com>
2020-09-24 15:34:27 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -11,3 +11,8 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  advise = "It's Dangerous To Go Alone! Take This."
+  puts advise
+end
